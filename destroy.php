2f690e3bccd59c68f23cf649d5c4464ad4ec01cf
Matthew Mantle <m.e.mantle@hud.ac.uk>
2025-10-24 11:50:44 +0100

    Modify destroy.php for film deletion logic
    Updated database connection details and changed SQL operation from UPDATE to DELETE.

--- destroy.php
+++ destroy.php
@@ -1,7 +1,7 @@
 <?php
 //Connect to the database
 try{
-    $conn = new PDO('mysql:host=localhost;dbname=MyDatabase', 'MyUsername', 'MyPassword');
+    $conn = new PDO('mysql:host=localhost;dbname=cit2202', 'root', '');
     $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
 }
 catch (PDOException $exception)
@@ -9,11 +9,17 @@ catch (PDOException $exception)
 	echo "Oh no, there was a problem" . $exception->getMessage();
 }
 
-//Get the id from the hidden fiedl in the form
+//Get the id from the hidden field in the form
 $id = $_POST['id'];
 
-//SQL UPDATE for updating a new row
+//SQL DELETE for deleting rows
+//First we need to delete the film's records from the junction table
 //Use a prepared statement to bind the id from the form
+$stmt = $conn->prepare("DELETE FROM film_genre WHERE film_genre.film_id = :id");
+$stmt->bindValue(':id',$id);
+$stmt->execute();
+
+//Now we can delete the film
 $stmt = $conn->prepare("DELETE FROM films WHERE films.id = :id");
 $stmt->bindValue(':id',$id);
 $stmt->execute();
